f7374376cd992c4a8397a22f5636f32d94d2bde2
pranto-ac <pranto.netmow@gmail.com>
2024-02-13 16:59:02 +0600

    t

--- inc/class-netmow-backup-admin.php
+++ inc/class-netmow-backup-admin.php
@@ -158,79 +158,11 @@
 		?>
 		<div class="nb-page-wrap">
             <h1>Netmow Backup</h1>
-			<div class="miyn-app-wrapper">
-			<div class="miyn-app-content-wrapper">
-				<div class="miyn-app-content-area">
-						My backup Plugins	
-				</div>
-			</div>
 			<div class="miyn-app-sidebar">
 				<form id="ajaxformid" action="#" method="POST">
 					<input type="submit" id="inviaForm" value="Send" name="Submit1">
 				</form>
 			</div>
-		</div>
-		<?php 
-			$db_values = get_option( 'netmow_google_keys' );
-			$clientid = '';
-			$clientsec = '';
-			$redirecturl = '';
-			if( $db_values ) {
-				$clientid = $db_values['clientid'] ? $db_values['clientid'] : '';
-				$clientsec = $db_values['clientsec'] ? $db_values['clientsec'] : '';
-				$redirecturl = $db_values['redirecturl'] ? $db_values['redirecturl'] : '';
-			}
-		?>
-		<div class="api-inputs">
-			<form method="post">
-				<label>Client ID <input type="text" name="clientid" value="<?php echo !empty($clientid) ? $clientid : $clientid; ?>"></label><br>
-				<label>Client secret <input type="text" name="clientsec" value="<?php echo !empty($clientsec) ? $clientsec : $clientsec; ?>"></label><br>
-				<label>Authorised redirect URI <input type="text" name="redirecturl" value="<?php echo !empty($redirecturl) ? $redirecturl : $redirecturl; ?>"></label><br>
-				<input type="submit" name="x_submit" value="Submit">
-			</form>
-		</div>
-
-		<div class="api-auth" style="padding: 20px;margin-top: 20px;background-color: #fff">
-			<h2>Google Account</h2>
-			<div class="panel panel-default">
-
-			<?php 
-				include WP_PLUGIN_DIR . '/miyn-backup/net-config.php';
-
-				$acc_valus = get_option( 'netmow_google_account_data' );
-				$g_access_token = '';
-				$user_first_name = '';
-				$user_last_name = '';
-				$user_email_address = '';
-				$user_image = '';
-				if( $acc_valus ) {
-					$g_access_token = $acc_valus['g_access_token'] ? $acc_valus['g_access_token'] : '';
-					$user_first_name = $acc_valus['user_first_name'] ? $acc_valus['user_first_name'] : '';
-					$user_last_name = $acc_valus['user_last_name'] ? $acc_valus['user_last_name'] : '';
-					$user_email_address = $acc_valus['user_email_address'] ? $acc_valus['user_email_address'] : '';
-					$user_image = $acc_valus['user_image'] ? $acc_valus['user_image'] : '';
-				}
-			?>
-
-			<?php
-			if(!empty($g_access_token))
-			{
-				echo '<div class="panel-heading">Welcome User</div><div class="panel-body">';
-				echo '<img src="'.$user_image.'" class="img-responsive img-circle img-thumbnail" />';
-				echo '<h3><b>Name :</b> '.$user_first_name.' '.$user_last_name.'</h3>';
-				echo '<h3><b>Email :</b> '.$user_email_address.'</h3>';
-				echo '<form method="post">';
-				echo '<input type="submit" name="revoke" id="revoke" value="Logout" >';
-				echo '</form>';
-			}
-			else
-			{
-			 echo '<a href="' . $google_client->createAuthUrl() . '"><img src="' . get_template_directory_uri() . '/assets/img/google.png" /></a>';
-			}
-			?>
-			</div>
-
-		</div>
         </div>
 		<?php
 	}

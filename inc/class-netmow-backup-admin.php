de734c885bbc2633cbdeac1b41f9e9ab6c907d96
pranto-ac <pranto.netmow@gmail.com>
2024-02-14 11:18:06 +0600

    h2

--- inc/class-netmow-backup-admin.php
+++ inc/class-netmow-backup-admin.php
@@ -214,7 +214,7 @@
 			// $home_url=home_url();
 			// header('Location: /wp-admin/admin.php?page=netmow-backup');
 			// $this->add_header_xua();
-			var_dump($data);
+			var_dump($gdata);
 		
 			}
 		

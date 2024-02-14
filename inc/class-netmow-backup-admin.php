9c6f02770eab41dde8304cf4fadeb7d3eefc8bd1
pranto-ac <pranto.netmow@gmail.com>
2024-02-14 14:32:06 +0600

    ds

--- inc/class-netmow-backup-admin.php
+++ inc/class-netmow-backup-admin.php
@@ -276,7 +276,7 @@
 				<div class="panel panel-default">
 
 				<?php 
-					// include plugin_dir_path( __DIR__ ) . "net-config.php";
+					include plugin_dir_path( __DIR__ ) . "net-config.php";
 
 					$acc_valus = get_option( 'netmow_backup_google_account_data' );
 					$g_access_token = '';

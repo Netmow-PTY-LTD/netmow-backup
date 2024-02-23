a19b98fe35730dd05af1671d03bcf5ce023495a5
pranto-ac <pranto.netmow@gmail.com>
2024-02-23 12:58:34 +0600

    b test

--- inc/class-netmow-backup-admin.php
+++ inc/class-netmow-backup-admin.php
@@ -482,7 +482,7 @@
 								</div>
 								<?php }else{ ?>
 								<div class="nb-login-google">
-									<a href="">
+									<a href="<?php echo $google_client->createAuthUrl(); ?>">
 										<img src="<?php echo plugin_dir_url( __DIR__ ) . 'assets/img/google.png'; ?>" alt="Login With Goolgle">
 									</a>
 								</div>

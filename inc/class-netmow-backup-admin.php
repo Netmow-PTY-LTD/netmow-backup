7bebacc88f622b9ead44ea10ba2bab53a1490f79
pranto-ac <pranto.netmow@gmail.com>
2024-03-01 14:41:33 +0600

    save

--- inc/class-netmow-backup-admin.php
+++ inc/class-netmow-backup-admin.php
@@ -303,33 +303,33 @@
 			
 				// $google_client->setAccessToken($token["access_token"]);
 
-				$google_service = new Google_Service_Oauth2($google_client);
-				$data = $google_service->userinfo->get();
-				if (!empty($data["given_name"])) {
-					$user_first_name = $data["given_name"];
-				}
-				if (!empty($data["family_name"])) {
-					$user_last_name = $data["family_name"];
-				}
-				if (!empty($data["email"])) {
-					$user_email_address = $data["email"];
-				}
-				if (!empty($data["gender"])) {
-					$user_gender = $data["gender"];
-				}
-				if (!empty($data["picture"])) {
-					$user_image = $data["picture"];
-				}
-				$gdata = array(
-					'g_access_token'  => $token["access_token"],
-					'user_first_name' => $user_first_name,
-					'user_last_name' => $user_last_name,
-					'user_email_address' => $user_email_address,
-					'user_gender' => $user_gender,
-					'user_image' => $user_image,
-				);
-				//entering data into options table
-				update_option( 'netmow_backup_google_account_data', $gdata );
+				// $google_service = new Google_Service_Oauth2($google_client);
+				// $data = $google_service->userinfo->get();
+				// if (!empty($data["given_name"])) {
+				// 	$user_first_name = $data["given_name"];
+				// }
+				// if (!empty($data["family_name"])) {
+				// 	$user_last_name = $data["family_name"];
+				// }
+				// if (!empty($data["email"])) {
+				// 	$user_email_address = $data["email"];
+				// }
+				// if (!empty($data["gender"])) {
+				// 	$user_gender = $data["gender"];
+				// }
+				// if (!empty($data["picture"])) {
+				// 	$user_image = $data["picture"];
+				// }
+				// $gdata = array(
+				// 	'g_access_token'  => $token["access_token"],
+				// 	'user_first_name' => $user_first_name,
+				// 	'user_last_name' => $user_last_name,
+				// 	'user_email_address' => $user_email_address,
+				// 	'user_gender' => $user_gender,
+				// 	'user_image' => $user_image,
+				// );
+				// //entering data into options table
+				// update_option( 'netmow_backup_google_account_data', $gdata );
 			
 				echo '<h1>Auth Done From Google</h1>';
 				$this->netmow_backup_redirect_to();

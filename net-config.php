15598132e88e121845b8a97e697c99417dda6bab
pranto-ac <pranto.netmow@gmail.com>
2024-03-05 11:33:46 +0600

    vd

--- net-config.php
+++ net-config.php
@@ -40,7 +40,9 @@ $gt_values = get_option( 'netmow_backup_google_account_data' );
 if(!empty($gt_values['g_access_token'])){
     $google_client->fetchAccessTokenWithRefreshToken($gt_values['g_access_token']);
 }
-echo '<h1>'fetchAccessTokenWithRefreshToken($gt_values['g_access_token']).'</h1>';
+
+var_dump(fetchAccessTokenWithRefreshToken($gt_values['g_access_token']));
+
 if ($google_client->isAccessTokenExpired()) {
     echo '<div style="color: red">Exp</div>';
     
